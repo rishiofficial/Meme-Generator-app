a43f9793f995e82e470f3805ba7ca760a7c47907
rishikesh pandey <rishikeshpandey248@gmail.com>
2023-01-13 12:48:09 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            
+            <h2 className="header--title">Meme Generator</h2>
+           
+        </header>
+    )
+}
